79b6279b894682b93ac3dba23ad3b3d6146abb3d
TanushaVeraksa <54898478+TanushaVeraksa@users.noreply.github.com>
2021-05-08 01:35:23 +0300

    last

--- src/Pages/MovieReviews/MovieReviews.jsx
+++ src/Pages/MovieReviews/MovieReviews.jsx
@@ -4,29 +4,19 @@ import { useDispatch, useSelector } from "react-redux";
 import ShowsGallery from "../../components/ShowsGallery";
 import Pagination from "@material-ui/lab/Pagination";
 import { Select, MenuItem } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
 
 const ALL_GENDER = "All";
-
-const useStyles = makeStyles({
-  root: {
-     display: "flex",
-     flexDirection: "column",
-     justifyContent: "center", 
-     alignItems: "center",
-     padding: 25,
-  }
-})
-
+const ALL_YEARS = "All";
 
 
 function MovieReviews() {
-  const classes = useStyles()
   const dispatch = useDispatch();
   const [from, setFrom] = useState(0);
   const [count, setCount] = useState(10);
   const [genres, setGenres] = useState([]);
+  const [years, setYears] = useState([]);
   const [selectedGenre, setSelectedGenre] = useState(ALL_GENDER);
+  const [selectedYear, setSelectedYear] = useState(ALL_YEARS);
   const {shows, searchedShows} = useSelector((state) => state.show);
 
   const showPerPage = 8;
@@ -37,49 +27,74 @@ function MovieReviews() {
 
   useEffect(() => {
     if (shows) {
-      const genresSet = new Set(shows.flatMap((show) => show.genres));
+      const genresSet = new Set(shows.flatMap(show => show.genres));
+      const yearsSet = new Set(shows.map(show => new Date(show.premiered).getFullYear() + ""));
+      setYears([...yearsSet.values()]);
       setGenres([...genresSet.values()]);
       setCount(Math.ceil(shows.length / showPerPage));
     }
-  }, [dispatch, shows]);
-
+  }, [shows]);
 
 
   useEffect(() => {
-    if (shows) {
-      if(selectedGenre === ALL_GENDER) {
-        const filteredShows = shows.map((show) => (show));
-        console.log("ALL1");
-        console.log(filteredShows);
-        setCount(Math.ceil(shows.length / showPerPage));
+    if (searchedShows) {
+      if(selectedYear !== ALL_YEARS) {
+        if(selectedGenre !== ALL_GENDER) {
+          const filteredShows = shows.filter(show => show.premiered ? show.premiered.includes(selectedYear) : false);
+           dispatch(setSearchedShows(filteredShows));
+        } else {
+          const filteredShows = searchedShows.filter(show => show.premiered ? show.premiered.includes(selectedYear) : false);
+          if(JSON.stringify(searchedShows) !== JSON.stringify(filteredShows)) {
+           dispatch(setSearchedShows(filteredShows));
+        }
+        }
       }
       if(selectedGenre !== ALL_GENDER) {
         const filteredShows = shows.filter((show) =>show.genres.includes(selectedGenre));
-        console.log("NOALL");
-        console.log(filteredShows);
-        setCount(Math.ceil(filteredShows.length / showPerPage));
-      dispatch(setSearchedShows(filteredShows));
+        if(JSON.stringify(searchedShows) !== JSON.stringify(filteredShows)) {
+           dispatch(setSearchedShows(filteredShows));
+        }
+        
       } else {
-        dispatch(setSearchedShows(shows));
+        //  dispatch(setSearchedShows(shows));
       }
     }
-  }, [dispatch, genres, selectedGenre, shows]);
+  }, [dispatch, genres, selectedGenre, searchedShows, selectedYear, shows]);
+
+  useEffect(() => {
+    if(shows) {
+      dispatch(setSearchedShows(shows));
+    }
+  }, [dispatch, shows])
 
   const handleChange = (event, page) => {
     setFrom((page - 1) * showPerPage);
   };
 
-  const handleSelectChange = (event) => {
+  const handleSelectGenre = (event) => {
     setSelectedGenre(event.target.value);
   };
 
+  const handleSelectYear = (event) => {
+    setSelectedYear(event.target.value);
+  }
+
   return (
-    <div className={classes.root}>
-      <Select value={selectedGenre}  onChange={handleSelectChange}>
+    <div>
+      <Select value={selectedGenre}  onChange={handleSelectGenre}>
         <MenuItem value="All">All</MenuItem>
         {genres.map((genre) => (
           <MenuItem key={genre} value={genre}>
             {genre}
+          </MenuItem>
+        ))}
+      </Select>
+
+      <Select value={selectedYear}  onChange={handleSelectYear}>
+        <MenuItem value="All">All</MenuItem>
+        {years.map((year) => (
+          <MenuItem key={year} value={year}>
+            {year}
           </MenuItem>
         ))}
       </Select>
